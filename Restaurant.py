2a6ffa2793ab5a20920840680d235c220928b081
Joseph Karanja <joseph.karanja@student.moringaschool.com>
2023-09-04 09:47:55 +0300

    complete challenge

---
+++ Restaurant.py
@@ -0,0 +1,10 @@
+class Restaurant:
+    all_restaurants = []
+
+    def __init__(self,name="",):
+        self.name = name
+        Restaurant.all_restaurants.append(self)
+
+    @classmethod
+    def all(cls):
+        return cls.all_restaurants
